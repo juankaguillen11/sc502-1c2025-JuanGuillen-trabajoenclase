a8d104b940198dfeb4e57e11c454ec588d65e036
juankaguillen11 <135542623+juankaguillen11@users.noreply.github.com>
2025-02-06 19:36:52 -0600

    semana 4 calculadora.1

--- Semana 4/script.js
+++ Semana 4/script.js
@@ -1,9 +1,9 @@
-function calculate(){
+function calculate() {
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
-    let operation =document.getElementById("operation").value;
-    
-    switch(operation){
+    let operation = document.getElementById("operation").value;
+
+    switch (operation) {
         case 'sum':
             result = num1 + num2;
             break;
@@ -14,7 +14,11 @@ function calculate(){
             result = num1 + num2;
             break;
         case 'div':
-            result = num1/num2;
+            if (num2 == 0) {
+                alert("No se puede dividir entre cero");
+            } else {
+                result = num1 / num2;
+            }
             break;
         default:
             alert("La operacion seleccionada no existe");
